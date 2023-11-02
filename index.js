f1287526be34fd5c5ac4e9c9687366aff795fe13
Xyear <021ryanvicente@gmail.com>
2023-11-02 19:22:09 -0300

    segundo  commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function mostrarSenha() {
+    var inputPass = document.querySelector('#senha')
+    var btnShowPass = document.querySelector('#olho')
+
+    if(inputPass.type === 'password') {
+        inputPass.setAttribute('type','text')
+        btnShowPass.classList.replace('bi-eye-fill','bi-eye-slash-fill')
+    } else{
+        inputPass.setAttribute('type','password')
+        btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill')
+    }
+}
+function mostrarSenhar() {
+    var inputPass = document.querySelector('#senhar')
+    var btnShowPass = document.querySelector('#olhor')
+
+    if(inputPass.type === 'password') {
+        inputPass.setAttribute('type','text')
+        btnShowPass.classList.replace('bi-eye-fill','bi-eye-slash-fill')
+    } else{
+        inputPass.setAttribute('type','password')
+        btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill')
+    }
+}
